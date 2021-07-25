5d25736e44717b0b299f060d8f55aee967db3077
sirisha kalivarapu <kalivarapusirisha62017@gmail.com>
2021-07-25 16:04:57 +0530

    FirstCommit

---
+++ src/test/java/Challenge4Balls.java
@@ -0,0 +1,52 @@
+
+
+import processing.core.PApplet;
+
+public class Challenge4Balls extends PApplet {
+
+    public static final int WIDTH = 1000;
+    public static final int HEIGHT = 1000;
+    public static final float diameter=10;
+    int x=0;
+    int y=HEIGHT/8;
+    private int i;
+
+
+    public static void main(String args[]){
+        PApplet.main("Challenge4Balls",args);
+    }
+
+    @Override
+    public void settings() {
+        super.settings();
+
+        size(WIDTH, HEIGHT);
+
+    }
+
+    @Override
+    public void setup() {
+        super.setup();
+        System.out.println("setup method");
+
+    }
+
+    @Override
+    public void draw() {
+        for ( i = 1; i <=4 ; i++) {
+            drawCircle();
+        }
+    }
+
+    private void drawCircle() {
+
+        ellipse(i*x/2,y*i, diameter, diameter);
+        x++;
+
+    }
+
+    private void paintbg() {
+        background(148,0,211);
+    }
+
+}
